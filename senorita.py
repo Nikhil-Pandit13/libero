57062fef9581a69b6b8220b26ba3ca897633e408
nikhil <nikhilpandit1310@gmail.com>
2024-05-15 13:21:30 +0530

    updated website URL

--- senorita.py
+++ senorita.py
@@ -1,10 +1,1 @@
-import webbrowser
 
-def open_google():
-    url = "https://www.facebook.com"
-    # Specify the path to Chrome executable if it's not in the default location
-    chrome_path = "C:/Program Files/Google/Chrome/Application/chrome.exe %s"
-    webbrowser.get(chrome_path).open(url)
-
-if __name__ == "__main__":
-    open_google()
