feff0740c5b4dfd50c66516f77d2e3f5ad500804
nikhil <nikhilpandit1310@gmail.com>
2024-05-16 23:10:19 +0530

    class + inheritance

--- senorita.py
+++ senorita.py
@@ -1,1 +1,23 @@
+#example of class
+class area():
+    def __init__(self, l):
+        self.l = l
 
+    def square(self):
+        return self.l * self.l
+
+
+david = area(5)
+print(david.square())
+
+#example of inheritance of prev class
+class volume(area):
+    def __init__(self, l, h):
+        self.l = l
+        self.h = h
+
+    def vol(self):
+        return self.l*self.l*self.h
+
+john = volume(6, 2)
+print(john.square(), john.vol())
